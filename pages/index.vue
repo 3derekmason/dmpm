5520939a1be6bf92ab0d29731f30d98490770065
Derek Mason <3derekmason@gmail.com>
2022-09-07 10:09:01 -0600

    chore: fetch packages on mount

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial />
+  <div class="home">
+    <h1>DPM</h1>
+    <ul>
+      <li v-for="(item, i) in packages" :key="i">{{ JSON.stringify(item) }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      packages: [],
+    }
+  },
+  mounted() {
+    this.getAllPackages()
+  },
+  methods: {
+    getAllPackages() {
+      fetch('https://dev-mail.vercel.app/api/packages')
+        .then((res) => res.json())
+        .then((data) => {
+          this.packages = JSON.parse(data.message)
+        })
+    },
+  },
 }
 </script>
