a4f19ce935ffe80692238c535ad1852caa573799
Derek Mason <3derekmason@gmail.com>
2022-09-07 10:32:34 -0600

    chore: declare tile props and component init

---
+++ components/PackageTile.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="tile">
+    <p>{{ name }}</p>
+    <p>{{ description }}</p>
+    <p>{{ version }}</p>
+    <p>{{ downloads }}</p>
+    <a :href="npm" target="_blank">View on NPM</a>
+    <a :href="github" target="_blank">View on Github</a>
+    <p>{{ created }}</p>
+    <p>{{ updated }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PackageTile',
+  props: {
+    name: {
+      type: String,
+      default: 'Name',
+    },
+    description: {
+      type: String,
+      default: 'About this package',
+    },
+    version: {
+      type: String,
+      default: '0.0.0',
+    },
+    downloads: {
+      type: Number,
+      default: 0,
+    },
+    npm: {
+      type: String,
+      default: 'https://npmjs.com/',
+    },
+    github: {
+      type: String,
+      default: 'https://github.com/',
+    },
+    created: {
+      type: Date,
+      default() {
+        return new Date()
+      },
+    },
+    updated: {
+      type: Date,
+      default() {
+        return new Date()
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tile {
+  border: 1px solid blue;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 4px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,29 @@
   <div class="home">
     <h1>DPM</h1>
     <ul>
-      <li v-for="(item, i) in packages" :key="i">{{ JSON.stringify(item) }}</li>
+      <PackageTile
+        v-for="(item, i) in packages"
+        :key="i"
+        :name="item.name"
+        :description="item.description"
+        :version="item.version"
+        :downloads="Number(item.downloads)"
+        :npm="item.npmURL"
+        :github="item.githubURL"
+        :created="item.created"
+        :updated="item.updated_at"
+      />
     </ul>
   </div>
 </template>
 
 <script>
+import PackageTile from '../components/PackageTile.vue'
 export default {
   name: 'IndexPage',
+  components: {
+    PackageTile,
+  },
   data() {
     return {
       packages: [],
