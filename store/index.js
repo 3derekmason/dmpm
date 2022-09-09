dc0e1aa778d97c107d7e836eba32737eb74e2837
Derek Mason <3derekmason@gmail.com>
2022-09-09 12:34:06 -0600

    revert: remove filter from store

--- store/index.js
+++ store/index.js
@@ -1,22 +1,15 @@
 export const state = () => ({
   search: '',
-  filter: '',
 })
 
 export const getters = {
   getSearch(state) {
     return state.search
   },
-  getFilter(state) {
-    return state.filter
-  },
 }
 
 export const mutations = {
   setSearch(state, string) {
     state.search = string
-  },
-  setFilter(state, string) {
-    state.filter = string.toLowerCase()
   },
 }
