131bc1857f51b5ea614ea5defc78232d22c3e259
Derek Mason <3derekmason@gmail.com>
2022-09-14 13:26:37 -0600

    fix: lighthouse seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Derek Mason Javascript Package Manger DMPM',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
